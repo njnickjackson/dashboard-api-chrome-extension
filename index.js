9114c027a6e57c8be2809f9b7b0fb168455fbda8
Nick Jackson <njnickjackson@yahoo.com>
2022-11-09 21:50:26 -0500

    updated background image quality

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
+        console.log(data.urls)
+        document.body.style.backgroundImage = `url(${data.urls.full})`
 		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     .catch(err => {
